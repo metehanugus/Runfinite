64b01461add656775857e12b1452f3aa2b102153
metehanugus <65510727+metehanugus@users.noreply.github.com>
2023-12-14 23:06:22 +0300

    karakter alma eklendi
    eve karakteri 50 golda alınıyor, karakter alınmadan oyuna girmiyor. İlk karakter default olarak açık geliyor.

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Character
+{
+    public int id;
+    public string name;
+    public int price;
+    public bool isUnlocked;
+}
+
 public class ShopManager : MonoBehaviour
 {
     public int currentCharacterIndex;
     public GameObject[] characterModels;
+    public Character[] characters;
+
     void Start()
     {
         currentCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
@@ -13,6 +24,10 @@ public class ShopManager : MonoBehaviour
             character.SetActive(false);
 
         characterModels[currentCharacterIndex].SetActive(true);
+        foreach (Character character in characters)
+        {
+            character.isUnlocked = PlayerPrefs.GetInt("CharacterUnlocked_" + character.id, character.isUnlocked ? 1 : 0) == 1;
+        }
     }
 
     // Update is called once per frame
@@ -44,4 +59,25 @@ public class ShopManager : MonoBehaviour
         characterModels[currentCharacterIndex].SetActive(true);
         PlayerPrefs.SetInt("SelectedCharacter", currentCharacterIndex);
     }
+
+    public void PurchaseCharacter(int characterIndex)
+    {
+        // index kontrolu
+        if (characterIndex < 0 || characterIndex >= characters.Length)
+        {
+            Debug.LogError("Character index is out of range: " + characterIndex);
+            return;
+        }
+        Character characterToPurchase = characters[characterIndex];
+        if (!characterToPurchase.isUnlocked && GameManager.inst.playerMoney >= characterToPurchase.price)
+        {
+            GameManager.inst.AddMoney(-characterToPurchase.price);
+            characterToPurchase.isUnlocked = true;
+
+            // kilit acildi kaydet
+            PlayerPrefs.SetInt("CharacterUnlocked_" + characterToPurchase.id, characterToPurchase.isUnlocked ? 1 : 0);
+            PlayerPrefs.Save();
+        }
+    }
+
 }
