f8f599addd306dce4808cc58d1ab4f2740d2093e
metehanugus <65510727+metehanugus@users.noreply.github.com>
2023-12-22 04:48:54 +0300

    more bugfix
    yine fixledik bişiler

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,6 +1,7 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Character
@@ -9,6 +10,7 @@ public class Character
     public string name;
     public int price;
     public bool isUnlocked;
+
 }
 
 public class ShopManager : MonoBehaviour
@@ -16,18 +18,23 @@ public class ShopManager : MonoBehaviour
     public int currentCharacterIndex;
     public GameObject[] characterModels;
     public Character[] characters;
+    public Button buyButton;
+
 
     void Start()
     {
         currentCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
-        foreach(GameObject character in characterModels)
+        foreach (GameObject character in characterModels)
             character.SetActive(false);
 
         characterModels[currentCharacterIndex].SetActive(true);
         foreach (Character character in characters)
         {
+            // PlayerPrefs'ten karakterin kilidini açma durumunu al
             character.isUnlocked = PlayerPrefs.GetInt("CharacterUnlocked_" + character.id, character.isUnlocked ? 1 : 0) == 1;
         }
+        UpdateBuyButtonVisibility();
+        UpdateBuyButtonEvent();
     }
 
     // Update is called once per frame
@@ -46,6 +53,8 @@ public class ShopManager : MonoBehaviour
 
         characterModels[currentCharacterIndex].SetActive(true);
         PlayerPrefs.SetInt("SelectedCharacter", currentCharacterIndex);
+        UpdateBuyButtonVisibility();
+        UpdateBuyButtonEvent();
     }
 
     public void ChangePrevious()
@@ -58,26 +67,63 @@ public class ShopManager : MonoBehaviour
 
         characterModels[currentCharacterIndex].SetActive(true);
         PlayerPrefs.SetInt("SelectedCharacter", currentCharacterIndex);
+        UpdateBuyButtonVisibility();
+        UpdateBuyButtonEvent();
     }
 
     public void PurchaseCharacter(int characterIndex)
     {
-        // index kontrolu
+        // Karakter indeksinin doğru olduğundan emin ol
         if (characterIndex < 0 || characterIndex >= characters.Length)
         {
             Debug.LogError("Character index is out of range: " + characterIndex);
             return;
         }
-        Character characterToPurchase = characters[characterIndex];
-        if (!characterToPurchase.isUnlocked && GameManager.inst.playerMoney >= characterToPurchase.price)
-        {
-            GameManager.inst.AddMoney(-characterToPurchase.price);
-            characterToPurchase.isUnlocked = true;
 
-            // kilit acildi kaydet
-            PlayerPrefs.SetInt("CharacterUnlocked_" + characterToPurchase.id, characterToPurchase.isUnlocked ? 1 : 0);
-            PlayerPrefs.Save();
+        // Seçilen karakteri al
+        Character characterToPurchase = characters[characterIndex];
+
+        // Eğer karakter zaten açıksa veya yeterli para yoksa işlemi durdur
+        if (characterToPurchase.isUnlocked)
+        {
+            Debug.Log("Bu karakter zaten açık.");
+            return;
         }
+        if (GameManager.inst.playerMoney < characterToPurchase.price)
+        {
+            Debug.Log("Yeterli paranız yok.");
+            return;
+        }
+
+        // Paranın yeterli olduğunu kontrol et ve karakter için öde
+        GameManager.inst.AddMoney(-characterToPurchase.price);
+        characterToPurchase.isUnlocked = true;
+
+        // Yalnızca satın alınan karakter için PlayerPrefs ayarını güncelle
+        PlayerPrefs.SetInt("CharacterUnlocked_" + characterToPurchase.id, 1);
+
+        // Diğer karakterlerin ayarlarını değiştirme
+        PlayerPrefs.Save();
+        UpdateBuyButtonVisibility();
+        UpdateBuyButtonEvent();
+    }
+
+
+    public void UpdateBuyButtonVisibility()
+    {
+        if (currentCharacterIndex < 0 || currentCharacterIndex >= characters.Length)
+        {
+            Debug.LogError("Geçersiz karakter indeksi: " + currentCharacterIndex);
+            return;
+        }
+        buyButton.gameObject.SetActive(!characters[currentCharacterIndex].isUnlocked);
+    }
+
+    public void UpdateBuyButtonEvent()
+    {
+        
+        buyButton.onClick.RemoveAllListeners(); 
+        buyButton.onClick.AddListener(() => PurchaseCharacter(currentCharacterIndex)); // Yeni event listener ekle
     }
 
 }
